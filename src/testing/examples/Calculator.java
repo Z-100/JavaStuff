b0cb1b01a1a50c91341275dde262baec71469b2b
marvin.zeiter <marvin.zieter@suisa.ch>
2021-10-21 15:14:29 +0200

    Testing

---
+++ src/testing/examples/Calculator.java
@@ -0,0 +1,8 @@
+package testing.examples;
+
+public class Calculator {
+
+    public int add(int i, int j) {
+        return i + j;
+    }
+}
---
+++ src/testing/examples/MyFirstJUnitJupiterTests.java
@@ -0,0 +1,15 @@
+package testing.examples;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MyFirstJUnitJupiterTests {
+
+    private final Calculator myCalculator = new Calculator();
+
+    @Test
+    void add() {
+        assertEquals(7, myCalculator.add(5, 2));
+    }
+}
