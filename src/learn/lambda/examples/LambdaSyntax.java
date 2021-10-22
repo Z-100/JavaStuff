1c6d9475bc63cf52e38c73a22ae3d71cee929075
Marvin <marvin131t@gmail.com>
2021-10-22 11:37:15 +0200

    Lambdas

--- src/learn/lambda/examples/LambdaSyntax.java
+++ src/learn/lambda/examples/LambdaSyntax.java
@@ -2,15 +2,24 @@ package learn.lambda.examples;
 
 public class LambdaSyntax {
 
-	public String method() {
-		return "Hello World";
+	public static void main(String[] args) {
+		System.out.println(method("Hello World"));
+
+		lambda.method("Hello World");
+
+		lambdaWithMethodReference.method("Hello World");
 	}
 
+	// ? Imperative way
+	private static String method(String s) {
+		return s;
+	}
+	// ? Lambda
+	private static Lambda lambda = (s) -> System.out.println(s);
+	// ? Cooler lambda
+	private static Lambda lambdaWithMethodReference = System.out::println;
 
-	// Same thing
-	Lambda lambda = (s) -> System.out.println(s);
-	Lambda lambdaWithMethodReference = System.out::println;
-
+	// ? Interface used by the lambda
 	interface Lambda {
 		void method(String s);
 	}
